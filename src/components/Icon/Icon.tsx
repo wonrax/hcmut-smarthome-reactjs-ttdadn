4d0b221ca3d7cc138ac133f35b0be2d25ae9845f
wonrax <hahuylonghai2012@gmail.com>
2021-05-05 21:03:09 +0700

    icon modifidable size

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,15 +1,20 @@
 import { Icons } from "../icons";
 import styles from "./Icon.module.css";
 
-export const Icon = (props: { icon: string; iconBackground?: boolean }) => {
+export const Icon = (props: {
+  icon: string;
+  iconBackground?: boolean;
+  size?: number;
+}) => {
   const IconComp: React.FunctionComponent<
     React.SVGProps<SVGSVGElement> & {
       title?: string | undefined;
     }
   > = Icons[props.icon];
+  const passingProps = props.size && { width: props.size, height: props.size };
   return (
     <div className={props.iconBackground ? styles["icon-w-bg"] : styles.icon}>
-      <IconComp />
+      <IconComp {...passingProps} />
     </div>
   );
 };
