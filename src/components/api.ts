ccc0c62ae0bb57dbbce0e435f18f981bb946eb52
wonrax <hahuylonghai2012@gmail.com>
2021-06-17 21:11:23 +0700

    HOTFIX websocket url

--- src/components/api.ts
+++ src/components/api.ts
@@ -1,3 +1,4 @@
 export const baseURL = "http://52.163.187.182/api";
 export const testUser = "0123456789";
-export const websocketURL = "ws://52.163.187.182/api/@" + testUser + "/devices";
+export const websocketURL =
+  "ws://52.163.187.182:8000/api/@" + testUser + "/devices";
