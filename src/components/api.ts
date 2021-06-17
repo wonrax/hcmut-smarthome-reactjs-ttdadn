ba79d68ae4802d0901a035875be49fe23ac2846c
wonrax <hahuylonghai2012@gmail.com>
2021-06-17 20:29:03 +0700

    staging url

--- src/components/api.ts
+++ src/components/api.ts
@@ -1,5 +1,3 @@
-// export const baseURL = "http://52.163.187.182/api";
-export const baseURL = "http://localhost:8000/api";
+export const baseURL = "http://52.163.187.182/api";
 export const testUser = "0123456789";
-// export const testUser = "Real_Device";
 export const websocketURL = "ws://localhost:8000/api/@" + testUser + "/devices";
