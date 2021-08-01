a04117da47a67841b3693350e272bde5f9e18362
wonrax <hahuylonghai2012@gmail.com>
2021-08-01 13:01:42 +0700

    change back port

--- src/components/api.ts
+++ src/components/api.ts
@@ -1,4 +1,4 @@
 export const baseURL = "http://52.230.82.36/api";
 export const testUser = "0123456789";
 export const websocketURL =
-  "ws://52.230.82.36:3000/api/@" + testUser + "/devices";
+  "ws://52.230.82.36:8000/api/@" + testUser + "/devices";
